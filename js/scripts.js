21eb63097888d0a14a14ea668ba92549c38c308b
Tomasz Kacprzak <monkhtc@gmail.com>
2017-03-20 20:52:08 +0100

    Zadanie 10.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,27 @@
 function Button(text) {
 	this.text = text || 'Hello';
-
+	
+}
 
 	Button.prototype = {
 		create: function() {
 				var self = this;
+
 				this.$element = $('<button>');
 				this.$element.text(this.text);
 				this.$element.click(function() {
 					alert(self.text);
+				});
 				this.$element.appendTo($('body'));
-					});
 		}
-
+		
 	}
 
-	var btn1 = new Button('Hello');
+	var btn1 = new Button('love');
+		btn2 = new Button('hate');
 
 	btn1.create();
-}
+	btn2.create();
 
 
 
